36b2fa82285843c3785845e3005c0276d3d1f883
omerbiton <User@DESKTOP-AKF78K4>
2019-09-13 21:54:42 +0300

    f

--- stack.c
+++ stack.c
@@ -6,12 +6,12 @@ struct Stack {
     int top;
     unsigned capacity;
     int* array;
-};
+}Stack;
 
 /* create a stack of a given capacity. It initializes size of
  * stack as 0
  * */
-struct Stack* createStack(unsigned capacity)
+Stack* createStack(unsigned capacity)
 {
     struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
     checkAllocatedMemory("malloc", stack);
@@ -20,21 +20,21 @@ struct Stack* createStack(unsigned capacity)
     stack->array = (int*)malloc(stack->capacity * sizeof(int));
     checkAllocatedMemory("malloc", stack->array);
     return stack;
-}Stack;
+}
 
-// Stack is full when top is equal to the last index
+/* Stack is full when top is equal to the last index */
 int isFull(Stack* stack)
 {
     return stack->top == stack->capacity - 1;
 }
 
-// Stack is empty when top is equal to -1
+/* Stack is empty when top is equal to -1 as in the begining */
 int isEmpty(Stack* stack)
 {
     return stack->top == -1;
 }
 
-// Function to add an item to stack.  It increases top by 1
+/* Increasing top by 1 and then add an item to stack at the new top position */
 void push(Stack* stack, int item)
 {
     if (isFull(stack))
@@ -42,7 +42,7 @@ void push(Stack* stack, int item)
     stack->array[++stack->top] = item;
 }
 
-// Function to remove an item from stack.  It decreases top by 1
+/* Decreasing top by 1 and then remove an item from stack at the new top position */
 int pop(Stack* stack)
 {
     if (isEmpty(stack))
@@ -50,7 +50,7 @@ int pop(Stack* stack)
     return stack->array[stack->top--];
 }
 
-// Function to return the top from stack without removing it
+/* return the top item from the stack without removing it */
 int peek(Stack* stack)
 {
     if (isEmpty(stack))
